f9dbaf06e462c63426c664106d4545e6d6823e45
szarbk <168341916+szarbk@users.noreply.github.com>
2024-06-28 11:36:53 -0900

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:derby://localhost:1527/shop";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
