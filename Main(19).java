aaa251549b5bfbc3e38d0841268b2ee0e482cc66
Negin-MOSAEI <153435392+Negin-MOSAEI@users.noreply.github.com>
2024-06-28 09:19:10 -0900

    Add files via upload

---
+++ Main(19).java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+public class Main{
+    public static void main(String[] args) {
+        Product_list p=new Product_list();
+    }
+}
